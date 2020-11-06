b94a3fa4b21df6d6b29c92e038ba2f2384434429
CVade <christian.vadevoulis@gmail.com>
2020-11-06 17:52:58 -0600

    Final HW-6 push

--- Assets/Javascript/index.js
+++ Assets/Javascript/index.js
@@ -6,6 +6,7 @@ $(document).ready(function() {
     if(localStorage.getItem("favoriteCities")){getWeather(JSON.parse(localStorage.getItem("favoriteCities"))[0]);}
 
     function getWeather(cityName){
+        $("#five-day-forecast").html("");
         var apiKey = "79a8a48bb56352d1877343ca1adaab77"
     
         var queryUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`
@@ -14,7 +15,6 @@ $(document).ready(function() {
             method: "GET",
             url: queryUrl
         }).then(function(res){
-            console.log(res)
             var lat = res.coord.lat
             var lon = res.coord.lon
             var city = res.name1996
@@ -22,16 +22,28 @@ $(document).ready(function() {
             var oneCallUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=hourly&units=imperial&appid=${apiKey}`
         
             $.get(oneCallUrl).then(function(data){
-                console.log('data from onecall api');
-                console.log(data);
+                var colorUVI = "";
+                if (data.current.uvi <= 2) {
+                    colorUVI = "greenUVI";
+                }
+                else if (data.current.uvi <= 5) {
+                    colorUVI = "yellowUVI";
+                }
+                else if (data.current.uvi <= 7) {
+                    colorUVI = "orangeUVI";
+                }
+                else {
+                    colorUVI = "redUVI";
+                }
+
                 $("#today-data").html(`
-                    <h4 class="card-title">${cityName} (${currentDay})</h4>
+                    <h4 class="card-title">${cityName} (${currentDay}) <img src="http://openweathermap.org/img/wn/${data.current.weather[0].icon}@2x.png" alt='weather icon' width='90' height='90' margin='auto'"/></h4>
                     <div class="card-text">
                         <ul>
                             <li>Temperature: ${data.current.temp} °F</li>
                             <li>Ηumidity: ${data.current.humidity}%</li>
                             <li>Wind Speed: ${data.current.wind_speed} MPH</li>
-                            <li>UV Index: ${data.current.uvi}</li>
+                            <li>UV Index: <span class="${colorUVI}">${data.current.uvi}</span></li>
                         </ul>
                     </div>`)
                 
@@ -42,8 +54,9 @@ $(document).ready(function() {
                         <h6 class="card-title">${date.getMonth()+1}/${date.getDate()}/${date.getFullYear()}</h4>
                         <div class="card-text">
                             <ul>
-                                <li>Temp Range: ${data.daily[i].temp.max}-${data.daily[i].temp.min} °F</li>
-                                <hr>
+                                <img src="http://openweathermap.org/img/wn/${data.daily[i].weather[0].icon}@2x.png" alt='weather icon' width='75' height='75' margin='auto'"/>
+                                <li>T-Max: ${data.daily[i].temp.max} °F</li>
+                                <li>T-Min: ${data.daily[i].temp.min} °F</li>
                                 <li>Ηumidity: ${data.daily[i].humidity}%</li>
                             </ul>
                         </div>
@@ -55,7 +68,6 @@ $(document).ready(function() {
 
     $("#search-btn").click(function(event){
         event.preventDefault();
-        $("#five-day-forecast").html("");
         var cityInput = $("#inlineFormInputCity").val();
         var favoriteCity = localStorage.getItem("favoriteCities");
         
@@ -75,25 +87,19 @@ $(document).ready(function() {
         getWeather(cityInput);
         favoriteCity = localStorage.setItem('favoriteCities', JSON.stringify(cityArr));
         updateRecent();
-
-        // check if you have localstorage with that name
-            // var stringArr =  JSON.stringify(["Chicago", "Denver"])
-            // write to localstorage
-        // read from localstorage
-        // var arr = JSON.parse()
-        // Add cityInput to arr
-        // write this arr to localstorage
-        
-        // print this to the page
     })
 
     function updateRecent() {
         let faveCities = JSON.parse(localStorage.getItem("favoriteCities"));
         $("#search-history").empty();
         for (var j = 0; j < faveCities.length; j++) {
-            $("#search-history").append("<div>" + faveCities[j] + "</div>");
+            $("#search-history").append("<div class='citySearch'>" + faveCities[j] + "</div>");
         }
     }
 
     updateRecent();
+
+    $("#search-history").on("click", "div", function() {
+        getWeather($(this).text());
+    });
 })
